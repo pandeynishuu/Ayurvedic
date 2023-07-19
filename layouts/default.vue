e9a6586bd22f32e66cc51db8d98b9387bcecd796
pandeynishuu <pandeynishuuu@gmail.com>
2023-07-19 11:59:33 +0545

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <header>
+            <NavbarComponent></NavbarComponent>
+        </header>
+        <main>
+            <slot></slot>
+        </main>
+        <footer>
+            <FooterComponent></FooterComponent>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
